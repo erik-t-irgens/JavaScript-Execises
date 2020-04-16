c666069e0d329260dc282daa029c3ce6233c38a5
Erik Irgens <erik.t.irgens@gmail.com>
2020-04-15 17:12:21 -0700

    [Improvement] - The string-reveral.js functionality has been updated to work correctly. [Fix] - The string passed in must be split before changing the character positions [Improvement] - Various comments have been added, to excess, for explanation of each change.

--- string-reversal.js
+++ string-reversal.js
@@ -1,22 +1,32 @@
-var args = process.argv.slice(2);
+const stringReversal = (text) => {
 
+    // The string must be split before you can modify its contents, but you can still avoid instanitating a new variable by directly modifying it like so.
+    text = text.split('')
 
-
-const stringReversal = (string) => {
+    // You only need to iterate over half of the string, as it replaced both sides at once. The .floor is for uneven strings, in which the middle-most character will stay the same regardless.
     let limit = Math.floor(text.length / 2)
 
     for (let i = 0; i < limit; i++) {
+
+        // Imagine that i is being used as the swapFROMLocation, or the beginning of the string. swapATLocation is the end of the string. Just a position.
         let swapAtLocation = text.length - i - 1;
+
+        // Two new variables are instantiated to hold the value of the string. In shorter string parameters, this has larger memory overhead as opposed to just creating a new string variable with a for loop that goes backwards. However, in longer strings, this is negligible. 
+
         let firstLetter = text[i]
         let secondLetter = text[swapAtLocation]
 
         text[i] = secondLetter;
         text[swapAtLocation] = firstLetter
+
+        console.log('switching ' + firstLetter + ' with ' + secondLetter)
     }
 
-    console.log(text)
-    return text
+
+    return text.join('')
 }
 
-var text = args[0].toString()
-text = stringReversal(text)
+// Arguments passed in through the console, which begin at the 2 index.
+const args = process.argv.slice(2);
+let text = args.toString()
+console.log('Final result: ' + stringReversal(text))
